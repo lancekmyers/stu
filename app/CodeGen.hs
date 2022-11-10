f930f4cddf8f15fb59aaabc37da0a4c70b7c0bb4
lancekmyers <lancekmyers@gmail.com>
2022-11-10 15:31:08 -0600

    Reorganised and modularised codegen

--- app/CodeGen.hs
+++ app/CodeGen.hs
@@ -1,34 +1,35 @@
-{-# LANGUAGE FlexibleContexts, TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
 
 module CodeGen  where
 
 import AST
-import Analysis
-import Data.String (IsString)
 import Data.Text (Text)
-import qualified Data.Text as T
 import qualified Data.Vector as V
 import Text.Builder (Builder)
-import qualified Text.Builder as Builder
 import Types
 import Control.Comonad.Trans.Cofree (CofreeF(..), tailF)
-import Data.Functor.Foldable
-
+import Data.Functor.Foldable ( fold, Recursive(cata) )
 import Data.String (IsString (..))
-import Control.Monad.Free (_Free)
-import Control.Monad.Reader (Reader, MonadReader (ask, local), runReader)
+import Control.Monad.Reader ( runReader ) 
 import Data.Maybe (mapMaybe)
 import Data.Functor.Identity (Identity(..))
 import Data.Functor.Compose (Compose(..))
 
-{-
-Preamble should look like 
+import CodeGen.Python
+    ( PyExp(..),
+      PyCode(..),
+      prettyCode,
+      jnp,
+      tfd,
+      tfb,
+      (@@),
+      (!$),
+      (!.),
+      (!) ) 
 
-from tensorflow_probability.substrates import jax as tfp
-import tfp.
--}
 
+preamble :: PyCode
 preamble = PyBlock [
   PyImport "tensorflow_probability.substrates.jax as tfp",
   PyImport "tensorflow_probability.substrates.jax.distributions as tfd", 
@@ -56,114 +57,6 @@ cgBij = fold (alg . tailF . runIdentity . getCompose) where
   alg (MkBij name args) = (tfb name) @@ (PyNum . Left <$> args)
     
   alg (Chain bs) = tfb "Chain" @@ [PyList bs]
-
-
--- necessary libraries
--- preamble :: Builder 
--- preamble = $(embedStringFile "app/preamble.txt")
-
-data PyCode 
-  = PyBlock [PyCode] -- maybe use DList
-  | PyAssign Text PyExp  
-  | PyDestr  [Text] PyExp 
-  | PyDef (Maybe Text) Text [Text] PyCode
-  | PyFor Text PyExp PyCode
-  | PyDo PyExp 
-  | PyRet PyExp 
-  | PyImport Text
-  deriving Show
-data PyExp 
-  = PyApply  PyExp [PyExp] [(Text, PyExp)]
-  | PyMethod PyExp Text [PyExp]
-  | PyIdent [Text] Text -- python identifier
-  | PyList [PyExp]
-  | PyDict [(Text, PyExp)]
-  | PyNum (Either Double Int)
-  | PyGet PyExp PyExp
-  | PyDot PyExp Text
-  | PyStr Text
-  deriving Show 
-
-instance IsString PyExp where
-  fromString = PyIdent [] . T.pack 
-
-prettyExp :: PyExp -> Builder
-prettyExp (PyIdent [] ident) = Builder.text ident 
-prettyExp (PyIdent modules ident) = 
-  (Builder.intercalate "." $ Builder.text <$> modules) <> "." <> Builder.text ident
-prettyExp (PyList xs) = "[" <> Builder.intercalate "," (prettyExp <$> xs) <> "]"
-prettyExp (PyDict xs) = "{" <> Builder.intercalate "," 
-  ["'" <> Builder.text k <> "'" <> " : " <> prettyExp v | (k, v) <- xs] <> "}"
-prettyExp (PyNum (Right i)) = Builder.decimal i
-prettyExp (PyNum (Left f)) = Builder.string $ show f
-prettyExp (PyGet x i) = prettyExp x <> "[" <> prettyExp i <> "]"
-prettyExp (PyDot x i) = prettyExp x <> "." <> Builder.text i
-prettyExp (PyStr txt) = "'" <> Builder.text txt <> "'"
-prettyExp (PyApply f xs []) = fname <> "(" <> args <> ")"
-  where 
-    fname = prettyExp f
-    args   = Builder.intercalate "," (prettyExp <$> xs) 
-prettyExp (PyApply f [] kws) = fname <> "(" <> kwArgs <> ")"
-  where 
-    fname = prettyExp f
-    kwArgs = Builder.intercalate "," 
-      [Builder.text kw <> "=" <> prettyExp v | (kw,v) <- kws]
-prettyExp (PyApply f xs kws) = fname <> "(" <> args <> "," <> kwArgs <> ")"
-  where 
-    fname = prettyExp f
-    args   = Builder.intercalate "," (prettyExp <$> xs) 
-    kwArgs = Builder.intercalate "," 
-      [Builder.text kw <> "=" <> prettyExp v | (kw,v) <- kws]
-prettyExp (PyMethod x f xs) = (prettyExp x) <> "." <> 
-  (Builder.text f) <> "(" <> Builder.intercalate "," (prettyExp <$> xs) <> ")"
-
-
-indent :: Builder -> Reader Int Builder
-indent b = do 
-  n <- ask 
-  pure $ Builder.text (T.replicate n "  ") <> b 
-
-prettyCode :: PyCode -> Reader Int Builder 
-prettyCode (PyBlock stmts) = Builder.intercalate "\n" <$> 
-  traverse prettyCode stmts 
-prettyCode (PyDo exp) = indent $ prettyExp exp
-prettyCode (PyAssign name val) = indent $ 
-  Builder.text name <> " = " <> prettyExp val 
-prettyCode (PyDestr names val) = indent $ 
-  (Builder.intercalate ", " . fmap Builder.text $ names) <> " = " <> prettyExp val 
-prettyCode (PyRet x) = indent $ "return (" <> prettyExp x <> ")"
-prettyCode (PyImport x) = indent $ "import " <> Builder.text x
-prettyCode (PyFor x iter body) = do 
-  line <- indent $ "for " <> Builder.text x <> " in " <> prettyExp iter <> ":" 
-  body <- local (+1) $ prettyCode body
-  return $ line <> "\n" <> body
-prettyCode (PyDef Nothing name args body) = do  
-  decl <- indent $ "def " <> Builder.text name <> 
-    "(" <> (Builder.intercalate ", " . fmap Builder.text $ args) <> "):"
-  body <- local (+1) $ prettyCode body
-  return $ decl <> "\n" <> body
-prettyCode (PyDef (Just dec) name args body) = do 
-  decorator <- indent $ "@" <> Builder.text dec <> "\n"
-  decl <- indent $ "def " <> Builder.text name <> 
-    "(" <> (Builder.intercalate ", " . fmap Builder.text $ args) <> "):"
-  body <- local (+1) $ prettyCode body
-  return $ decorator <> decl <> "\n" <> body
-
-jnp :: Text -> PyExp
-jnp name = PyIdent ["jnp"] name
-tfd :: Text -> PyExp
-tfd name = PyIdent ["tfd"] name
-tfb :: Text -> PyExp
-tfb name = PyIdent ["tfb"] name
-(@@) :: PyExp -> [PyExp] -> PyExp
-f @@ x = PyApply f x []
-
-(!$) :: PyExp -> Text -> [PyExp] -> PyExp 
-x !$ f = PyMethod x f  
-(!.) :: PyExp ->Text -> PyExp 
-x !. i = PyDot x i
-(!) :: PyExp -> PyExp -> PyExp 
-x ! i = PyGet x i
 
 
 cgExpr :: Expr w -> PyExp
@@ -264,7 +157,6 @@ cgData = PyAssign "data" (obser_data !$ "__or__" $ [const_data])
       [PyIdent ["jnp"] "array", x]
     obser_data = map_jnp_array ("dict" @@ ["inf_obj" !. "observed_data"])
     const_data = map_jnp_array ("dict" @@ ["inf_obj" !. "constant_data"])
-
 
 
 cgPriorPredGo :: (ModelStmt a) -> Int -> PyCode 
