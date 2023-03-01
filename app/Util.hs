82180f66a17ecb196f2001ee9f454b607992e3d9
lancekmyers <lancekmyers@gmail.com>
2023-02-28 19:41:13 -0600

    I had totally misunderstood how the position type works

--- app/Util.hs
+++ app/Util.hs
@@ -7,13 +7,10 @@ type SrcSpan = Position
 
 mkPos :: SourcePos -> SourcePos -> SrcSpan
 mkPos (SourcePos file l c) (SourcePos _ l' c') 
-    = Position (unPos l, unPos l') (unPos c, unPos c') file
+    = Position (unPos l, unPos c) (unPos l', unPos c') file
 
 joinSrcSpan :: SrcSpan -> SrcSpan -> SrcSpan 
 joinSrcSpan 
-    (Position (l11, l12) (c11, c12) fname) 
-    (Position (l21, l22) (c21, c22) _) 
-    = Position (l, l') (c, c') fname 
-    where 
-        (l,  c)  = (l11, c11) `min` (l21, c21)
-        (l', c') = (l12, c12) `max` (l22, c22)
+    (Position start _stop fname) 
+    (Position _start stop _) 
+    = Position start stop fname 
