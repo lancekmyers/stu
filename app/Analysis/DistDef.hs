d4e45f9e89d8cda93552a87268d1c67ed99e3b22
lancekmyers <lancekmyers@gmail.com>
2023-03-21 22:26:47 -0500

    Fix issue with the compiler not knowing about functions in sampler

--- app/Analysis/DistDef.hs
+++ app/Analysis/DistDef.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Analysis.DistDef where
 
@@ -7,29 +9,28 @@ module Analysis.DistDef where
 import AST
   ( DistDef (DistDef),
     Distribution (Distribution),
+    FunDef (FunDef),
     SampleBody (..),
-    VarDomain (Local, Bound),
-    FunDef (FunDef)
+    VarDomain (Bound, Local),
   )
-import Analysis.Context (MonadTyCtx, insertDist, insertTy, validateType, introCards, introCardsFromTy)
+import Analysis.Context (Ctx, MonadTyCtx, insertDist, insertTy, introArgs, introCards, introCardsFromTy, validateType)
 import Analysis.Distribution (inferTyDist)
 import Analysis.Error
 import Analysis.Expr (inferTy)
-import Analysis.FunDef (checkFunDefs, checkFunDef)
+import Analysis.FunDef (checkFunDef, checkFunDefs)
 import Control.Comonad.Identity (Identity (runIdentity))
 import Control.Comonad.Trans.Cofree (Cofree, headF)
 import Control.Monad (when)
+import Control.Monad.Reader (MonadReader (ask), runReaderT)
+import Control.Monad.Reader.Class (MonadReader (local))
+import Control.Monad.State.Strict (MonadState (get))
+import Control.Monad.Validate (MonadValidate)
 import Data.Functor.Compose (Compose (getCompose))
 import Data.Functor.Foldable (Recursive (project))
-import qualified Data.Set as S 
+import qualified Data.Set as S
 import qualified Data.Vector as V
-import Types (FunctionTy (FunctionTy), Ty, shape, broadcastsTo, Shape (getVec))
-import Control.Monad.Reader.Class (MonadReader(local))
-import Control.Monad.State.Strict (MonadState (get))
-import Analysis.Context (Ctx)
-import Control.Monad.Reader (runReaderT, MonadReader (ask))
+import Types (FunctionTy (FunctionTy), Shape (getVec), Ty, broadcastsTo, shape)
 import Util (SrcSpan)
-import Control.Monad.Validate (MonadValidate)
 
 cofreeHead :: Functor f => Cofree f a -> a
 cofreeHead = headF . runIdentity . getCompose . project
@@ -40,20 +41,20 @@ checkDistDefs dists = traverse go dists
     go :: DistDef SrcSpan -> m (DistDef Ty)
     go dist = do
       ctx <- get
-      dist'@(DistDef name args eventTy lpdf sample bij) <- 
+      dist'@(DistDef name args eventTy lpdf sample bij) <-
         runReaderT (checkDistDef dist) ctx
       insertDist name (FunctionTy args eventTy) (const () <$> bij)
       return dist'
 
 checkDistDef ::
+  forall m.
   (MonadTyCtx m) =>
   DistDef SrcSpan ->
   m (DistDef Ty)
 checkDistDef (DistDef name args eventTy lpdf sample bij) = do
-  ctx <- ask   
+  ctx <- ask
   lpdf' <- checkFunDef lpdf
-  let insertArgs = mconcat [insertTy x Bound t . introCardsFromTy t | (x, t) <- args]
-  (eventTy', sample') <- local insertArgs $ checkSample sample
+  (eventTy', sample') <- local (introArgs args) $ checkSample sample
   -- let err = expectedGot eventTy eventTy'
   when (eventTy' /= eventTy) $ doesNotMatchReturnType eventTy eventTy'
   let bij' = (const eventTy) <$> bij
@@ -75,11 +76,11 @@ checkSample (SampleUnifIn name ty rest) = local (insertTy name Local ty) $ do
   validateType ty
   (eventTy, rest') <- checkSample rest
   return (eventTy, SampleUnifIn name ty rest')
-checkSample (SampleLetIn name ty val rest) = local (insertTy name Local ty) $ do
+checkSample (SampleLetIn name ty val rest) = do
   validateType ty
   val' <- inferTy val
   let ty' = cofreeHead val'
   -- let err = expectedGot ty ty'
   when (not $ ty' `broadcastsTo` ty) $ doesNotMatchDeclaredType ty ty'
-  (eventTy, rest') <- checkSample rest
+  (eventTy, rest') <- local (insertTy name Local ty) $ checkSample rest
   return (eventTy, SampleLetIn name ty val' rest')
